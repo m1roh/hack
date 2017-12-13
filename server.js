8a407740b14be1d1049593015e03e5e6fd1c2ea7
Romain Haye <hayromain.rh@gmail.com>
2017-12-13 02:53:51 +0100

    test

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var apiai = require('apiai');
 
 var app = apiai("c2e976879b1a4f9c822185d7caf30888");
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
 var request = app.textRequest('Je suis invité ce soir', {
   sessionId: '201c816f-6e4a-446e-b7df-6feae9a5d8cd'
 });
